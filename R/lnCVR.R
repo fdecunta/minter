bf190bf56d3407a45238994c1055bdc67bd3d83a
Facundo Decunta <fdecunta@agro.uba.ar>
2025-05-10 16:13:02 -0300

    Add more tests

--- R/lnCVR.R
+++ R/lnCVR.R
@@ -1,3 +1,94 @@
+#' Log Coefficient Of Variation Ratio (lnCVR)
+#' 
+#' @param type Type of effect size: "ind", "main", or "inter"
+#' @param data Data frame with the data
+#' @param col_names Vector of two strings to name the output columns for the effect size and it's sampling variance. Default is 'yi' and 'vi'.
+#' @param append Logical. Append the results to \code{data}. Default is TRUE
+#' @param Ctrl_mean Mean outcome from the Control treatment
+#' @param Ctrl_sd Standard deviation from the control treatment
+#' @param Ctrl_n Sample size from the control streatment
+#' @param A_mean Mean outcome from the treatment
+#' @param A_sd Standard deviation from the treatment
+#' @param A_n Sample size from the treatment
+#' @param B_mean Mean outcome from the B treatment
+#' @param B_sd Standard deviation from the B treatment
+#' @param B_n Sample size from the B treatment
+#' @param AB_mean Mean outcome from the interaction AxB treatment
+#' @param AB_sd Standard deviation from the interaction AxB treatment
+#' @param AB_n Sample size from the interaction AxB treatment
+#'
+#' @author Facundo Decunta - fdecunta@agro.uba.ar
+#'
+#' @export
+lnCVR <- function(
+  type,
+  data,
+  col_names = c("yi", "vi"),
+  append = TRUE,
+  Ctrl_mean,
+  Ctrl_sd,
+  Ctrl_n,
+  A_mean,
+  A_sd,
+  A_n,
+  B_mean = NULL,
+  B_sd = NULL,
+  B_n = NULL,
+  AB_mean = NULL,
+  AB_sd = NULL,
+  AB_n = NULL
+) {
+  checkmate::assert_choice(type, choices = c("ind", "main", "inter"))
+  checkmate::assert_character(col_names, len = 2)
+  checkmate::assert_logical(append, len = 1)
+  checkmate::assert_data_frame(data)
+  
+  # Define columns needed and evaluate them using 'data' to
+  # get the column vectors
+  if (type == "ind") {
+    vars <- list(
+      Ctrl_mean = substitute(Ctrl_mean),
+      Ctrl_sd   = substitute(Ctrl_sd),
+      Ctrl_n    = substitute(Ctrl_n),
+      A_mean    = substitute(A_mean),
+      A_sd      = substitute(A_sd),
+      A_n       = substitute(A_n)
+    )
+  } else {
+    vars <- list(
+      Ctrl_mean = substitute(Ctrl_mean),
+      Ctrl_sd   = substitute(Ctrl_sd),
+      Ctrl_n    = substitute(Ctrl_n),
+      A_mean    = substitute(A_mean),
+      A_sd      = substitute(A_sd),
+      A_n       = substitute(A_n),
+      B_mean    = substitute(B_mean),
+      B_sd      = substitute(B_sd),
+      B_n       = substitute(B_n),
+      AB_mean   = substitute(AB_mean),
+      AB_sd     = substitute(AB_sd),
+      AB_n      = substitute(AB_n)
+    )
+  }
+  effsize_args <- lapply(vars, function(x) eval(x, data))
+
+  # Pass arguments to the type of effect size to compute
+  fn <- switch(type,
+    ind = ".simple_lnCVR",
+    main = ".main_lnCVR",
+    inter = ".interaction_lnCVR"
+  )
+  df <- do.call(fn, effsize_args)
+
+  # Rename columns 
+  colnames(df) <- col_names
+
+  if (append) {
+    df <- cbind(data, df)
+  }
+
+  return(df)
+}
 
 
 
--- R/lnVR.R
+++ R/lnVR.R
@@ -1,3 +1,82 @@
+#' Log of Variation Ratio (lnVR)
+#' 
+#' @param type Type of effect size: "ind", "main", or "inter"
+#' @param data Data frame with the data
+#' @param col_names Vector of two strings to name the output columns for the effect size and it's sampling variance. Default is 'yi' and 'vi'.
+#' @param append Logical. Append the results to \code{data}. Default is TRUE
+#' @param Ctrl_sd Standard deviation from the control treatment
+#' @param Ctrl_n Sample size from the control streatment
+#' @param A_sd Standard deviation from the treatment
+#' @param A_n Sample size from the treatment
+#' @param B_sd Standard deviation from the B treatment
+#' @param B_n Sample size from the B treatment
+#' @param AB_sd Standard deviation from the interaction AxB treatment
+#' @param AB_n Sample size from the interaction AxB treatment
+#'
+#' @author Facundo Decunta - fdecunta@agro.uba.ar
+#'
+#' @export
+lnVR <- function(
+  type,
+  data,
+  col_names = c("yi", "vi"),
+  append = TRUE,
+  Ctrl_sd,
+  Ctrl_n,
+  A_sd,
+  A_n,
+  B_sd = NULL,
+  B_n = NULL,
+  AB_sd = NULL,
+  AB_n = NULL
+) {
+  checkmate::assert_choice(type, choices = c("ind", "main", "inter"))
+  checkmate::assert_character(col_names, len = 2)
+  checkmate::assert_logical(append, len = 1)
+  checkmate::assert_data_frame(data)
+  
+  # Define columns needed and evaluate them using 'data' to
+  # get the column vectors
+  if (type == "ind") {
+    vars <- list(
+      Ctrl_sd   = substitute(Ctrl_sd),
+      Ctrl_n    = substitute(Ctrl_n),
+      A_sd      = substitute(A_sd),
+      A_n       = substitute(A_n)
+    )
+  } else {
+    vars <- list(
+      Ctrl_sd   = substitute(Ctrl_sd),
+      Ctrl_n    = substitute(Ctrl_n),
+      A_sd      = substitute(A_sd),
+      A_n       = substitute(A_n),
+      B_sd      = substitute(B_sd),
+      B_n       = substitute(B_n),
+      AB_sd     = substitute(AB_sd),
+      AB_n      = substitute(AB_n)
+    )
+  }
+  effsize_args <- lapply(vars, function(x) eval(x, data))
+
+  # Pass arguments to the type of effect size to compute
+  fn <- switch(type,
+    ind = ".simple_lnVR",
+    main = ".main_lnVR",
+    inter = ".interaction_lnVR"
+  )
+  df <- do.call(fn, effsize_args)
+
+  # Rename columns 
+  colnames(df) <- col_names
+
+  if (append) {
+    df <- cbind(data, df)
+  }
+
+  return(df)
+}
+
+
 #' Simple Log Variability ratio
 #'
 #' This is computed using formulas from Nakagawa et al. 2015
